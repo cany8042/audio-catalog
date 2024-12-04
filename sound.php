badd0450b97d5b94596f051d3dfd655764a74715
Mi-stik31 <121046700+Mi-stik31@users.noreply.github.com>
2024-12-04 20:49:12 +0300

    Added display name of track. Closed #9 (#15)

--- sound.php
+++ sound.php
@@ -4,26 +4,27 @@ class Sound implements JsonSerializable
 {
     private $_filePath;
 
-    public function __construct($filePath)
+    private $_name;
+
+    public function __construct($filePath, $name)
     {
         $this->_filePath = $filePath;
+        $this->_name = $name;
     }
 
     public function jsonSerialize(): array
     {
-        return ['filePath' => $this->_filePath];
+        return ['filePath' => $this->_filePath, 'name' => $this->_name];
     }
 
     public static function getSongsFromDirectory(): array
     {
         $sounds = [];
-        $allFiles = new DirectoryIterator(dirname(__FILE__)."/sounds");
+        $allFiles = new DirectoryIterator(dirname(__FILE__) . "/sounds");
 
-        foreach ($allFiles as $soundFile)
-        {
-            if ($soundFile->isFile())
-            {
-                $sound = new Sound("sounds/{$soundFile->getFilename()}");
+        foreach ($allFiles as $soundFile) {
+            if ($soundFile->isFile()) {
+                $sound = new Sound("sounds/{$soundFile->getFilename()}", "example");
                 $sounds[] = $sound;
             }
         }
@@ -32,7 +33,6 @@ class Sound implements JsonSerializable
     }
 }
 
-if($_SERVER['REQUEST_METHOD'] == 'GET')
-{
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     echo json_encode(Sound::getSongsFromDirectory(), JSON_UNESCAPED_UNICODE);
 }
